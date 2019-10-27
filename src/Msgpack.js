379654ea8157049d9d9b0cac4b72ef39499161b3
Ping Chen <remotenonsense@gmail.com>
2019-10-27 17:59:06 +0900

    Replace Uint8Array with ArrayBuffer for interoperability with libs

--- src/Msgpack.js
+++ src/Msgpack.js
@@ -1,48 +1,52 @@
-var msgpack = require("@msgpack/msgpack");
+const msgpack = require("@msgpack/msgpack");
 
-var CHUNK_SIZE = 65536;
+const CHUNK_SIZE = 65536;
 
 function uint8ArrayToString(arr) {
   if (arr.length < CHUNK_SIZE) {
     return String.fromCharCode.apply(null, arr);
   }
 
-  var result = "";
-  var arrLen = arr.length;
-  for (var i = 0; i < arrLen; i++) {
-    var chunk = arr.subarray(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE);
+  const result = "";
+  const arrLen = arr.length;
+  for (let i = 0; i < arrLen; i++) {
+    const chunk = arr.subarray(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE);
     result += String.fromCharCode.apply(null, chunk);
   }
   return result;
 }
 
 function stringToUint8Array(str) {
-  var buf = new ArrayBuffer(str.length);
-  var arr = new Uint8Array(buf);
-  var strLen = str.length;
-  for (var i = 0; i < strLen; i++) {
+  const buf = new ArrayBuffer(str.length);
+  const arr = new Uint8Array(buf);
+  const strLen = str.length;
+  for (let i = 0; i < strLen; i++) {
     arr[i] = str.charCodeAt(i);
   }
   return arr;
 }
 
-var decode = function(left, right, arr) {
+function decode(left, right, arr) {
   try {
     return right(msgpack.decode(arr));
   } catch (e) {
     return left(e);
   }
-};
+}
 
-exports.encode_ = msgpack.encode;
+exports.encode_ = json => {
+  const encoded = msgpack.encode(json);
+  return encoded.buffer.slice(encoded.byteOffset, encoded.byteLength);
+};
 
 exports.encodeToString_ = function(json) {
   return uint8ArrayToString(msgpack.encode(json));
 };
 
-exports.decode_ = decode;
+exports.decode_ = left => right => buffer =>
+  decode(left, right, new Uint8Array(buffer));
 
-exports.decodeString_ = function(left, right, str) {
-  var arr = stringToUint8Array(str);
+exports.decodeString_ = left => right => str => {
+  const arr = stringToUint8Array(str);
   return decode(left, right, arr);
 };
--- src/Msgpack.purs
+++ src/Msgpack.purs
@@ -10,14 +10,13 @@ import Prelude
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode (class DecodeJson, decodeJson)
 import Data.Argonaut.Encode (class EncodeJson, encodeJson)
-import Data.ArrayBuffer.Types (Uint8Array)
+import Data.ArrayBuffer.Types (ArrayBuffer)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
-import Data.Function.Uncurried (Fn3, runFn3)
 import Effect.Exception (Error, error)
 
-foreign import encode_ :: Json -> Uint8Array
-encode :: forall a. EncodeJson a => a -> Uint8Array
+foreign import encode_ :: Json -> ArrayBuffer
+encode :: forall a. EncodeJson a => a -> ArrayBuffer
 encode = encode_ <<< encodeJson
 
 foreign import encodeToString_ :: Json -> String
@@ -25,17 +24,21 @@ encode' :: forall a. EncodeJson a => a -> String
 encode' = encodeToString_ <<< encodeJson
 
 foreign import decode_
-  :: Fn3 (Error -> Either Error Json)
-         (Json -> Either Error Json)
-         Uint8Array
-         (Either Error Json)
-decode :: forall a. DecodeJson a => Uint8Array -> Either Error a
-decode s = runFn3 decode_ Left Right s >>= lmap error <<< decodeJson
+  :: forall a
+   . (Error -> Either Error Json)
+  -> (Json -> Either Error Json)
+  -> ArrayBuffer
+  -> Either Error a
+
+decode :: forall a. DecodeJson a => ArrayBuffer -> Either Error a
+decode s = decode_ Left Right s >>= lmap error <<< decodeJson
 
 foreign import decodeString_
-  :: Fn3 (Error -> Either Error Json)
-         (Json -> Either Error Json)
-         String
-         (Either Error Json)
+  :: forall a
+   . (Error -> Either Error Json)
+  -> (Json -> Either Error Json)
+  -> String
+  -> Either Error a
+
 decode' :: forall a. DecodeJson a => String -> Either Error a
-decode' s = runFn3 decodeString_ Left Right s >>= lmap error <<< decodeJson
+decode' s = decodeString_ Left Right s >>= lmap error <<< decodeJson

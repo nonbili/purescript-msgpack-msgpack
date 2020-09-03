bdf28887c466ad3e560e7d41f8dd7c394855801d
Ping Chen <remotenonsense@gmail.com>
2020-09-03 14:28:29 +0900

    Use TextDecoder instead of uint8ArrayToString

--- src/Msgpack.js
+++ src/Msgpack.js
@@ -1,21 +1,5 @@
 const msgpack = require("@msgpack/msgpack");
 
-const CHUNK_SIZE = 65536;
-
-function uint8ArrayToString(arr) {
-  if (arr.length < CHUNK_SIZE) {
-    return String.fromCharCode.apply(null, arr);
-  }
-
-  let result = "";
-  const arrLen = arr.length;
-  for (let i = 0; i < arrLen; i++) {
-    const chunk = arr.subarray(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE);
-    result += String.fromCharCode.apply(null, chunk);
-  }
-  return result;
-}
-
 function stringToUint8Array(str) {
   const buf = new ArrayBuffer(str.length);
   const arr = new Uint8Array(buf);
@@ -37,7 +21,7 @@ function decode(left, right, arr) {
 exports.encode_ = msgpack.encode;
 
 exports.encodeToString_ = function(json) {
-  return uint8ArrayToString(msgpack.encode(json));
+  return new TextDecoder("latin1").decode(msgpack.encode(json));
 };
 
 exports.decode_ = left => right => arr => decode(left, right, arr);
